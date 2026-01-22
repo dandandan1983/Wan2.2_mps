add596bd850fe07c29ea855c9672badcfde82717
Dan <dandandan@list.ru>
2026-01-23 01:00:46 +0300

    Refactor lazy imports for speech2video and animate modules to handle ImportError gracefully

--- wan/__init__.py
+++ wan/__init__.py
@@ -1,7 +1,16 @@
 # Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
 from . import configs, distributed, modules
 from .image2video import WanI2V
-from .speech2video import WanS2V
 from .text2video import WanT2V
 from .textimage2video import WanTI2V
-from .animate import WanAnimate
+
+# Lazy imports for modules with extra dependencies
+try:
+    from .speech2video import WanS2V
+except ImportError:
+    WanS2V = None
+
+try:
+    from .animate import WanAnimate
+except ImportError:
+    WanAnimate = None
